9b3cb77bbbe38ab6bced03400153080fff0da57a
Jan Stanley Go <stango97@gmail.com>
2018-04-02 10:58:06 -0400

    Add files via upload

--- Software/lockerautoscript.py
+++ Software/lockerautoscript.py
@@ -1,25 +1,41 @@
 import urllib2, urllib
+import time
+import sys
+import datetime
+import re
 
-user_name=#withheld for security reasons
-user_pin=#withheld for security reasons
+user_name='Jeremy'
+user_pin='1996'
 
 prodstat1=4
 prodstat2=2
 
-path='http://munro.humber.ca/~n01116269/db234.php?user_name='+user_name+'&user_pin='+user_pin  #the url you want to POST to
+
+while True: 
+    print 'Checking product status'
+    path='http://munro.humber.ca/~n01116269/display.php?u_name='+user_name
+    
+    req=urllib2.Request(path)
+    req.add_header("Content-type", "application/x-www-form-urlencoded")
+
+    page=urllib2.urlopen(req).read()
+
+    notag = re.sub("<.*?>", " ", page)
+    
+    print notag
+    
+    time.sleep(5)
+    
+
 path2='http://munro.humber.ca/~n01116269/door.php?p_stat='+prodstat1+'&u_name='+user_name
 path3='http://munro.humber.ca/~n01116269/lock.php?p_stat='+prodstat2+'&u_name='+user_name
 
-req=urllib2.Request(path)
 req2=urlib2.Request(path2)
 req3=urlib2.Request(path3) 
 
-
-req.add_header("Content-type", "application/x-www-form-urlencoded")
 req2.add_header("Content-type", "application/x-www-form-urlencoded")
 req3.add_header("Content-type", "application/x-www-form-urlencoded")
 
-page=urllib2.urlopen(req).read()
 page2=urllib2.urlopen(req2).read()
 page3=urllib2.urlopen(req3).read()
 
